7082d47fba3235f6d92d350507a9cf564345a254
Brit Clousing <bclousing@spotx.tv>
2018-11-06 11:40:20 -0700

    Allow MySQL communication to happen over unix domain sockets

--- src/ConnectionInfo.cpp
+++ src/ConnectionInfo.cpp
@@ -6,7 +6,6 @@ namespace wing
 ConnectionInfo::ConnectionInfo(
     std::string host,
     uint16_t port,
-    std::string socket,
     std::string user,
     std::string password,
     std::string database,
@@ -14,6 +13,24 @@ ConnectionInfo::ConnectionInfo(
 )
     : m_host(std::move(host)),
       m_port(port),
+      m_socket("0"),
+      m_user(std::move(user)),
+      m_password(std::move(password)),
+      m_database(std::move(database)),
+      m_client_flags(client_flags)
+{
+
+}
+
+ConnectionInfo::ConnectionInfo(
+    std::string socket,
+    std::string user,
+    std::string password,
+    std::string database,
+    uint64_t client_flags
+)
+    : m_host("localhost"),
+      m_port(0),
       m_socket(std::move(socket)),
       m_user(std::move(user)),
       m_password(std::move(password)),
