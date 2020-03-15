64147a53a4b69f31e9e1ff9f1cca5556926f3a51
jbaldwin <joshrbaldwin@gmail.com>
2020-03-15 13:13:11 -0600

    Add CI

--- src/Query.cpp
+++ src/Query.cpp
@@ -34,8 +34,8 @@ Query::Query(
     uint64_t auto_reconnect = 1;
     mysql_options(&m_mysql, MYSQL_OPT_RECONNECT, &auto_reconnect);
 
-    bool ssl = false;
-    mysql_options(&m_mysql, MYSQL_OPT_SSL_ENFORCE, &ssl);
+    // bool ssl = false;
+    // mysql_options(&m_mysql, MYSQL_OPT_SSL_ENFORCE, &ssl);
 
     //  Some mysql libraries do not support SSL_MODE_DISABLED
 #ifdef WING_PERCONA_SSL_DISABLED
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <wing/WingMySQL.h>
+
+TEST_CASE("catch_is_working")
+{
+    CHECK(true);
+}
